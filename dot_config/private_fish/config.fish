fb9906380fbe4d59c458fafd5d81eeb0a3e40406
Julien ADAMEK <julien.adamek@suse.com>
2025-01-23 10:25:27 +0100

    Source asdf.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 
 export PATH="$PATH /opt/homebrew/bin" 
-source $HOME/.asdf/asdf.fish
+source /opt/homebrew/share/fish/vendor_completions.d/asdf.fish
 /opt/homebrew/bin/fzf --fish | source
 /opt/homebrew/bin/starship init fish | source
 
